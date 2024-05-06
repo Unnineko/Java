4cdfcea7da01963ae1690d84c5d736989be769c0
Unnineko <158840581+Unnineko@users.noreply.github.com>
2024-05-06 21:41:49 +0300

    15

---
+++ Laba15.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Laba15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String ip = input.nextLine();
+        String regex = "(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)){3}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ip);
+        
+        if (matcher.find()) {
+            System.out.println("Найденный IP-адрес: " + matcher.group());
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("ip_addresses.txt", true));
+                writer.write(matcher.group() + "\n");
+                System.out.println("IP-адрес успешно записан в файл.");
+            } catch (IOException e) {
+                System.err.println("Ошибка при записи в файл: " + e.getMessage());
+            }
+        } else {
+            System.out.println("IP-адрес не найден в строке.");
+        }
+    }
+}
