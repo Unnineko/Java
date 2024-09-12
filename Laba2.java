a41a006648fb3894371143addcf88c0bd8b404fb
Unnineko <158840581+Unnineko@users.noreply.github.com>
2024-09-12 16:15:56 +0300

    2

--- Laba2.java
+++ Laba2.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
-
-public class Laba2 {
+public class laba2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Введите значение x:");
-        double x = input.nextDouble();
-        System.out.println("Введите количество членов ряда:");
-        int n = input.nextInt();
-        double sum = 0;
-
-        for (int i=0; i<n; i++) {
-            sum += Math.pow(-1, i) * Math.pow(x, 2*i) / factorial(i);
+        int[] sequence = new int[10]; // Создаем массив для хранения последовательности
+        
+        // Заполняем последовательность неотрицательными целыми числами, не превышающими 10000
+        for (int i = 0; i < 10; i++) {
+            sequence[i] = (int) (Math.random() * 100);
         }
-        System.out.println("Сумма ряда: " + sum);
-    }
-
-    public static double factorial(int n) {
-        if (n == 0) {
-            return 1;
+        
+        int maxR = -1;
+        
+        // Проходим по всем элементам последовательности
+        for (int i = 0; i < 10; i++) {
+            for (int j = i+1; j < 10; j++) {
+                int result = sequence[i] * sequence[j];
+                
+                if (result % 14 == 0 && result > maxR) {
+                    maxR = result;
+                }
+            }
+        }
+        
+        if (maxR == -1) {
+            System.out.println("Число R не найдено.");
         } else {
-            return n * factorial(n-1);
+            System.out.println("Максимальное число R, кратное 14 и произведение 2х различных элементов последовательности: " + maxR);
         }
     }
 }
