4f6c6deb9f75a641686be423d563847ddf9e5dd8
Unnineko <158840581+Unnineko@users.noreply.github.com>
2024-09-13 00:30:54 +0300

    2

--- Laba2.java
+++ Laba2.java
@@ -1,29 +1,40 @@
+import java.util.HashSet;
+import java.util.Random;
+
 public class laba2 {
+
     public static void main(String[] args) {
-        int[] sequence = new int[10]; // Создаем массив для хранения последовательности
-        
-        // Заполняем последовательность неотрицательными целыми числами, не превышающими 10000
-        for (int i = 0; i < 10; i++) {
-            sequence[i] = (int) (Math.random() * 100);
+        final int arraySize = 10; // размер последовательности
+        final int maxValue = 100; // максимальное значение каждого случайного числа
+
+        Random random = new Random(); // объект random для генерации случайных чисел
+        HashSet<Integer> set = new HashSet<>(); // для хранения случайных чисел
+
+        while (set.size() < arraySize) { //размер множества должен быть меньше заданного размера
+            set.add(random.nextInt(maxValue) + 1); // генерация случайного числа от 1 до maxValue (включительно) и добавление его в set
         }
-        
-        int maxR = -1;
-        
-        // Проходим по всем элементам последовательности
-        for (int i = 0; i < 10; i++) {
-            for (int j = i+1; j < 10; j++) {
-                int result = sequence[i] * sequence[j];
-                
-                if (result % 14 == 0 && result > maxR) {
-                    maxR = result;
-                }
+        searchMaxProduct(set);
+    }
+    // метод для поиска максимального произведения среди чисел, которые кратны заданным значениям
+    public static void searchMaxProduct(HashSet<Integer> set) {
+        int max14 = -1; // для хранения максимального числа, кратного 14
+        int max2 = -1;  // для хранения максимального числа, кратного 2
+        int max7 = -1;  // для хранения максимального числа, кратного 7
+
+        for (Integer num : set) {
+            if (num % 14 == 0 && num > max14) {
+                max14 = num;
+            } else if (num % 2 == 0 && num > max2) {
+                max2 = num;
+            } else if (num % 7 == 0 && num > max7) {
+                max7 = num;
             }
         }
-        
-        if (maxR == -1) {
-            System.out.println("Число R не найдено.");
+        int maxProduct = Math.max(max14, (max2 != -1 && max7 != -1) ? max2 * max7 : -1);
+        if (maxProduct == -1) {
+            System.out.println("Не найдено подходящих чисел.");
         } else {
-            System.out.println("Максимальное число R, кратное 14 и произведение 2х различных элементов последовательности: " + maxR);
+            System.out.println("Максимальное произведение: " + maxProduct);
         }
     }
 }
